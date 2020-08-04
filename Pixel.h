addc805a0171582674259e061d3b6073acce56dc
niccolo <niccolo.zanieri.13@gmail.com>
2020-08-04 17:22:32 +0200

    Added Pixel interface

---
+++ Pixel.h
@@ -0,0 +1,14 @@
+//
+// Created by niccolo on 04/08/20.
+//
+
+#ifndef TEMPLATE_IMAGES_PIXEL_H
+#define TEMPLATE_IMAGES_PIXEL_H
+
+
+class Pixel {
+
+};
+
+
+#endif //TEMPLATE_IMAGES_PIXEL_H
--- PixelRGB.h
+++ PixelRGB.h
@@ -7,6 +7,7 @@
 
 
 class PixelRGB {
+
 private:
     float r;
     float g;
